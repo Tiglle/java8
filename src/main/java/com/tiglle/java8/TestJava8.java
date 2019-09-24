a0154023a843c4f3167c460e3fec83008facdd90
Tiglle <759249926@qq.com>
2019-09-24 13:58:14 +0800

    因为不能提交空文件夹，所以搞了个文件1

---
+++ src/main/java/com/tiglle/java8/TestJava8.java
@@ -0,0 +1,22 @@
+/*
+ * 文件名：TestJava8
+ * 版权：Copyright by 启海云仓 qihaiyun.com
+ * 描述：
+ * 创建人：Administrator
+ * 创建时间：2019/9/24
+ * 修改理由：
+ * 修改内容：
+ */
+package com.tiglle.java8;
+
+/**
+ * 〈一句话简述该类/接口的功能〉
+ * 〈功能详细描述〉
+ *
+ * @author Administrator
+ * @version 1.0
+ * @see TestJava8
+ * @since JDK1.8
+ */
+public class TestJava8 {
+}
